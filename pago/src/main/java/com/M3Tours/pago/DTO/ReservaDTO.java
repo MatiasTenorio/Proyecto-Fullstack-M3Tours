b429ea99de3414be6d779b971efcfebf1019f773
MatiT <mat.tenorio@duocuc.cl>
2026-05-21 05:02:54 -0400

    Feat: Microservicio Pago completo

---
+++ pago/src/main/java/com/M3Tours/pago/DTO/ReservaDTO.java
@@ -0,0 +1,8 @@
+package com.M3Tours.pago.DTO;
+
+import lombok.Data;
+
+@Data
+public class ReservaDTO {
+    private Integer id;
+}
---
+++ pago/src/main/resources/db/migration/V1__tabla_pagos.sql
@@ -0,0 +1,9 @@
+CREATE TABLE pago (
+    id Integer PRIMARY KEY AUTO_INCREMENT,
+    orden_compra Integer NOT NULL,
+    usuario_id Integer NOT NULL,
+    reserva_id Integer NOT NULL,
+    costo DOUBLE NOT NULL,
+    fecha_pago DATE,
+    fecha_emision DATE DEFAULT CURRENT_DATE
+);
