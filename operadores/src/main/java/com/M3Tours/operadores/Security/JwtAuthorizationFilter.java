deeedbfceefc8ac96398118b79f72af07d4650da
MatiT <mat.tenorio@duocuc.cl>
2026-06-18 02:55:41 -0400

    Feat (Operadores: Security Config)

--- operadores/src/main/java/com/M3Tours/operadores/Security/JwtAuthorizationFilter.java
+++ operadores/src/main/java/com/M3Tours/operadores/Security/JwtAuthorizationFilter.java
@@ -9,6 +9,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
 
